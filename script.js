ed9412501da18c66851b596c08c7d1a4a9e23e88
pdurfey <3durfey@gmail.com>
2023-01-10 23:22:44 -0500

    completed, need to work on speed as it freezes

--- script.js
+++ script.js
@@ -4,22 +4,32 @@ let container = document.getElementById("etch_container");
 newEtchSize(16);
 function newEtchSize(size)
 {
-    for(let x = 0; x < Math.pow(size, 2); x++)
+    let subElements = document.getElementsByClassName("box");
+    for(let x = subElements.length; x < Math.pow(size, 2); x++)
     {
-        htmlElements += ('<div class="box"></div>');
+        htmlElements += ('<div id="boxID" class="box"></div>');
     }
     container.innerHTML = htmlElements;
-
+    while(subElements.length > Math.pow(size, 2))
+    {
+        subElements[0].parentNode.removeChild(subElements[0]);
+    }
     //add hover function
-    let subElements = document.getElementsByClassName("box");
+    console.log(subElements.length);
     for(let x = 0; x < subElements.length; x++)
     {
         subElements[x].addEventListener("mouseover", (event) => {
         event.target.style.backgroundColor = "black";
         })
-        /*subElements[x].style.setProperty('--width', 150/this.value);
-        subElements[x].style.setProperty('--height', 150/this.value);*/
+        subElements[x].style.setProperty('width', 500/size + 'px');
+        subElements[x].style.setProperty('height', 500/size + 'px');
+
     }
+    const style = getComputedStyle(subElements[0]);
+    console.log("test value " + 500/size);
+    console.log("width " + style.width);
+    console.log("size " + size);
+    console.log("height " + style.height);
 }
 
 
@@ -33,8 +43,7 @@ sliderValue.innerHTML = slider.value;
 slider.oninput = function () 
 {
     sliderValue.innerHTML = this.value;
-    /*newEtchSize(this.value);*/
-    /*document.getElementById("etch_container").style.setProperty('--grid-template-columns', 'repeat(25, 1fr)');*/
-    
+    newEtchSize(this.value);
+    document.getElementById("etch_container").style.gridTemplateColumns = `repeat(${this.value}, 1fr)`;
 }
 
