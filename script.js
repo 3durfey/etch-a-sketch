6bd0b1ca81ae70fddc8cd9b3acd2a406c3fe164d
pdurfey <3durfey@gmail.com>
2023-01-10 21:15:39 -0500

    completed outline and most of project
    Needs to accept new value for grid

---
+++ script.js
@@ -0,0 +1,40 @@
+//add all divs
+let htmlElements = "";
+let container = document.getElementById("etch_container");
+newEtchSize(16);
+function newEtchSize(size)
+{
+    for(let x = 0; x < Math.pow(size, 2); x++)
+    {
+        htmlElements += ('<div class="box"></div>');
+    }
+    container.innerHTML = htmlElements;
+
+    //add hover function
+    let subElements = document.getElementsByClassName("box");
+    for(let x = 0; x < subElements.length; x++)
+    {
+        subElements[x].addEventListener("mouseover", (event) => {
+        event.target.style.backgroundColor = "black";
+        })
+        /*subElements[x].style.setProperty('--width', 150/this.value);
+        subElements[x].style.setProperty('--height', 150/this.value);*/
+    }
+}
+
+
+
+
+//add new size to etch-a-sketch
+let slider = document.getElementById("myRange");
+let sliderValue = document.getElementById("sliderValue");
+sliderValue.innerHTML = slider.value;
+
+slider.oninput = function () 
+{
+    sliderValue.innerHTML = this.value;
+    /*newEtchSize(this.value);*/
+    /*document.getElementById("etch_container").style.setProperty('--grid-template-columns', 'repeat(25, 1fr)');*/
+    
+}
+
