93f45d68b6819a045bfac59e0d4b5bbbd555d053
pdurfey <3durfey@gmail.com>
2023-01-12 04:11:06 -0500

    cleaned up code

--- script.js
+++ script.js
@@ -1,164 +1,71 @@
 
-//add all divs
+//container and each box in container
 let container = document.getElementById("etch_container");
-let htmlElements = "";
-let mouse = 0;
-let containerWidthAndHeight = 500;
-newEtchSize(16);
+let boxes = document.getElementsByClassName("box");
+let rainBowSwitch = document.getElementById("rainbowSwitch");
 
-function newEtchSize(size)
+function newEtchASketch()
 {
+    //set NumberOfBoxes of container
+    let containerWidthAndHeight = document.getElementById("containerNumberOfBoxes").value;
+    container.style.height = containerWidthAndHeight + "px";
+    container.style.width = containerWidthAndHeight + "px";
+    //set number of boxes
+    let NumberOfBoxes = document.getElementById("numberOfBoxes").value;
+    document.getElementById("txtInput").value = NumberOfBoxes;
+    document.getElementById("numberOfBoxes").value = NumberOfBoxes;
+    document.getElementById("etch_container").style.gridTemplateColumns = `repeat(${NumberOfBoxes}, 1fr)`;
     //reset colors
     reset();
     //add any needed boxes
-    let subElements = document.getElementsByClassName("box");
-    for(let x = subElements.length; x < Math.pow(size, 2); x++)
+    let htmlElements = "";
+    for(let x = boxes.length; x < Math.pow(NumberOfBoxes, 2); x++)
     {
         htmlElements += ('<div id="boxID" class="box"></div>');
     }
     container.innerHTML += htmlElements;
-    htmlElements = "";
-    //delete unwanted elements
-    while(subElements.length > Math.pow(size, 2))
+    //delete unwanted boxes
+    while(boxes.length > Math.pow(NumberOfBoxes, 2))
     {
-        subElements[0].parentNode.removeChild(subElements[0]);
+        boxes[0].parentNode.removeChild(boxes[0]);
     }
-    //add hover function
-    let rainBowSwitch = document.getElementById("rainbowSwitch");
-    for(let x = 0; x < subElements.length; x++)
+    //add properties to each element
+    for(let x = 0; x < boxes.length; x++)
     {
-        if(rainBowSwitch.checked == true)
-        {
-            changeColor(subElements[x], `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`);
-        }
-        else
-        {
-            changeColor(subElements[x], "black");
-        }
-        subElements[x].style.setProperty('width', containerWidthAndHeight/size + 'px');
-        subElements[x].style.setProperty('height', containerWidthAndHeight/size + 'px');
+        rainBowSwitch.checked ? changeColor(boxes[x], `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`) : changeColor(boxes[x], "black");
+        boxes[x].style.outlineStyle = (document.getElementById("gridLines").checked ? 'solid' : 'none');
+        boxes[x].style.setProperty('width', containerWidthAndHeight/NumberOfBoxes + 'px');
+        boxes[x].style.setProperty('height', containerWidthAndHeight/NumberOfBoxes + 'px');
+        boxes[x].style.borderRadius = document.getElementById("radiusBorder").value + 'px';
     }
-
-    function changeColor(boxElement, color)
+    //determine which mouse mode to apply to each box
+    let mouse = document.getElementById("buttonDown").checked ? 1 : 0;
+    function changeColor(box, color)
     {
-        const colorChangeEvent = event => event.target.style.backgroundColor = color;
-        boxElement.addEventListener("mousedown", colorChangeEvent);
-        
-        boxElement.addEventListener("mouseover", event => {
-            if (event.buttons == mouse) colorChangeEvent(event);
+        const colorChangeEvent = mouseEvent => mouseEvent.target.style.backgroundColor = color;
+        box.addEventListener("mousedown", colorChangeEvent);
+        box.addEventListener("mouseover", mouseEvent => {
+            if (mouseEvent.buttons == mouse) colorChangeEvent(mouseEvent);
             });
     }
 }
-
-function mouseDown()
-{
-    let mouseStatus = document.getElementById("buttonDown");
-    mouse = mouseStatus.checked ? 1 : 0;
-}
-function randomNumber()
-{
-    return Math.floor(Math.random() * (255));
-}
-
-function changeSize(input)
-{
-    if(parseInt(input.length) === 0 || input > 100 || parseInt(input) === 0)
-    {
-        let alert = document.getElementById("alert");
-        alert.innerHTML = "Invalid Entry. Must be between 1 and 100";
-        $("#alert").hide();
-        return;
-    }
-    document.getElementById("alert").innerHTML = "   ";
-    document.getElementById("txtInput").value = input;
-    document.getElementById("myRange").value = input;
-    document.getElementById("etch_container").style.gridTemplateColumns = `repeat(${input}, 1fr)`;
-    newEtchSize(input);
-}
+//random number for colors
+const randomNumber = () => Math.floor(Math.random() * (255));
+//change color from inputs
+const changeColor = (red, blue, green) => {
+    [].forEach.call(boxes, function(box) {box.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`})};
+//reset etch-a-sketch
 function reset()
 {    
-    let subElements = document.getElementsByClassName("box");
-    let red = document.getElementById("red");
-    let blue = document.getElementById("blue");
-    let green = document.getElementById("green");
-    red.value = 255;
-    blue.value = 192;
-    green.value = 203;
-    for(let x = 0; x < subElements.length; x++)
-    {
-        subElements[x].style.backgroundColor = 'pink';
-    }
-    RainbowOnOff()
-}
-
-function changeRadius(value)
-{
-    let boxes = document.getElementsByClassName("box");
-    for(let x = 0; x < boxes.length; x++)
-    {
-        boxes[x].style.borderRadius = value + 'px';
-    }
+    //reset color bars
+    let red = document.getElementById("red").value = 255;
+    let blue = document.getElementById("blue").value = 192;
+    let green = document.getElementById("green").value = 203;
+    //reset each box
+    [].forEach.call(boxes, function(box) {box.style.backgroundColor = 'pink',
+    box.style.borderRadius = document.getElementById("radiusBorder").value + 'px';});
 }
 
 
-function changeColor(red, blue, green)
-{
-    let boxes = document.getElementsByClassName("box");
-    for(let x = 0; x < boxes.length; x++)
-    {
-        boxes[x].style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
-    }
-}
-function RainbowOnOff()
-{
-    let rainBowSwitch = document.getElementById("rainbowSwitch");
-    let subElements = document.getElementsByClassName("box");
-    for(let x = 0; x < subElements.length; x++)
-    {
-        if(rainBowSwitch.checked == true)
-        {
-            changeColor(subElements[x], `rgb(${randomNumber()}, ${randomNumber()}, ${randomNumber()})`);
-        }
-        else
-        {
-            changeColor(subElements[x], "black");
-        }
-    }
 
-    function changeColor(boxElement, color)
-    {
-        const colorChangeEvent = event => event.target.style.backgroundColor = color;
-        boxElement.addEventListener("mousedown", colorChangeEvent);
 
-        boxElement.addEventListener("mouseover", event => {
-            if (event.buttons == mouse) colorChangeEvent(event);
-            });
-    }
-}
-
-function changeContainerSize(size)
-{
-    containerWidthAndHeight = size;
-    document.getElementById("etch_container").style.height = size + "px";
-    document.getElementById("etch_container").style.width = size + "px";
-    newEtchSize(document.getElementById("myRange").value);
-
-}
-
-function toggleGridLines()
-{
-    let gridLines = document.getElementById("gridLines");
-    let subElements = document.getElementsByClassName("box");
-    for(let x = 0; x < subElements.length; x++)
-    {
-        if(gridLines.checked == true)
-        {
-            subElements[x].style.outlineStyle = 'solid';
-        }
-        else
-        {
-            subElements[x].style.outlineStyle = 'none';
-        }
-    }
-    newEtchSize(document.getElementById("myRange").value);
-}
